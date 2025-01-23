2e2f2bdab09be30a41e73d7083247b9ed84eaa89
ulianchenko <ulianchenko@gmail.com>
2025-01-23 23:55:56 +0200

    initial commit

---
+++ components/events/event-list.js
@@ -0,0 +1,20 @@
+import EventItem from "./event-item";
+
+function EventList({ items }) {
+  return (
+    <ul>
+      {items.map((event) => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default EventList;
