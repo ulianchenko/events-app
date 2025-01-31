1e51d59e1dab2629256b1bfc61a4c65d2f4a3d86
ulianchenko <ulianchenko@gmail.com>
2025-01-31 02:44:16 +0200

    the mongodb database was added and react context api was added

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,26 @@
+import { useContext } from "react";
+
 import MainHeader from "./main-header";
+import Notification from "../ui/notification";
+import NotificationContext from "../../store/notification-context";
 
 function Layout({ children }) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
   return (
     <>
       <MainHeader />
-      <main>{children}</main>
+      <main>
+        {children}
+        {activeNotification && (
+          <Notification
+            title={activeNotification.title}
+            message={activeNotification.message}
+            status={activeNotification.status}
+          />
+        )}
+      </main>
     </>
   );
 }
